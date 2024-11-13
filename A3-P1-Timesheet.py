211abf96eca5f41027400673fdfe62e5622ea6e8
W0198745JC <w0198745@campus.nscc.ca>
2024-11-13 16:19:32 -0400

    almostdone

--- A3-P1-Timesheet.py
+++ A3-P1-Timesheet.py
@@ -3,47 +3,59 @@
 #               each of five work days from the user, then displays different 
 #               information calculated about those entries as output. 
 
-#Student #:     
-#Student Name:  
+#Student #:     W0198745
+#Student Name:  Jenille Cheney 
+
+# took me forever to remember that functions go OUTSIDE the main function 
+# gave them an underscore to create an empty perameter knowing with in the code it would refer to the list of the same name 
+def calculateAverageDay(_hoursWorked):
+        return sum(_hoursWorked)/len(_hoursWorked)
+    
+def printAvgDay(_hoursWorked):
+    print("----------------------------------------------------------")
+    print(f"The total number or hours worked was: {sum(_hoursWorked)}")
+    averageHours = calculateAverageDay(_hoursWorked)
+    print(f"The average number of hours worked each day was: {averageHours:.1f}")
+    print("----------------------------------------------------------")
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-# get input for for daily hours worked 
+    # get input for for daily hours worked / make it a loop 
     
     hoursWorked= []
-    def hours():
-        for counter in range(5):
+
+    for counter in range(5):
             hoursWorked.append(int(input(f"Enter Hours Worked on Day # {counter + 1}: ")))
-        return hoursWorked
-    hours()
-# seperate longest day(s) worked from list 
+        
+    #   seperate longest day(s) worked from list 
     longestDay=[]
-    def longDay():
-         for i in range(len(hoursWorked)):
+   
+    for i in range(len(hoursWorked)):
              if hoursWorked[i] == max(hoursWorked):    
                 longestDay.append(i)  #had listing the values changed to list the indexs so I could call back to the day from hours worked.
-#print longest day(s) 
-         print("----------------------------------------------------------")
-         print("The most hours worked was on:")
-         for i in range(len(longestDay)):    #turned this into a loop to make up for multiple days with the same max hours
-            print(f"Day{longestDay[i]+1} when you worked {hoursWorked[longestDay[i]]} hours")
-    return longestDay
+    #print longest day(s) 
     print("----------------------------------------------------------")
-    print(f"The total number or hours worked was: {sum(hoursWorked)}")
-    averageHours=sum(hoursWorked)/len(hoursWorked)
-    print(f"The average number of hours worked each day was: {averageHours:.1f}")
-    print("----------------------------------------------------------")
-# seperate Slack Days ( less than 7 hours)
+    print("The most hours worked was on:")
+    for i in range(len(longestDay)):    #turned this into a loop to make up for multiple days with the same max hours
+        print(f"Day{longestDay[i]+1} when you worked {hoursWorked[longestDay[i]]} hours")
+            
+    
+    printAvgDay(hoursWorked)  # FUNCTION with A RETURN !!!!!!!
+ 
+    # seperate Slack Days ( less than 7 hours)
     slackDays=[]
+    
     for i in range(len(hoursWorked)):
-         if hoursWorked[i]<7:
+            if hoursWorked[i]<7:
              slackDays.append(i)
     print("Days you slacked off (i.e. worked less than 7 hours): ")
     for i in range(len(slackDays)):
-        print(f"Day #{slackDays[i]+1}:  {hoursWorked[slackDays[i]]}")
+            print(f"Day #{slackDays[i]+1}:  {hoursWorked[slackDays[i]]}")
+   
 
+  
 
 # CODE WORKS WOOOOO now to add functions 
     # YOUR CODE ENDS HERE
-    hours()
+
 main()
